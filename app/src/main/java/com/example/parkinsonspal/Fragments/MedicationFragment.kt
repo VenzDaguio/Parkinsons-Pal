a47313ce1d55b2e0857d461217e7ffe926c475a9
Venz Daguio <paolodaguio@gmail.com>
2023-04-17 22:39:19 +0100

    Added the medication feature, now user can add medication and be set reminders

--- app/src/main/java/com/example/parkinsonspal/Fragments/MedicationFragment.kt
+++ app/src/main/java/com/example/parkinsonspal/Fragments/MedicationFragment.kt
@@ -1,60 +1,65 @@
 package com.example.parkinsonspal.Fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.parkinsonspal.R
+import android.widget.*
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.parkinsonspal.*
+import com.example.parkinsonspal.Model.DataBaseHelper
+import java.time.LocalTime
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class MedicationFragment() : Fragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MedicationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MedicationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var dbHelper: DataBaseHelper
+    private lateinit var adapter: MedicationAdapter
+    private lateinit var viewModel: TrackHealthViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        viewModel = ViewModelProvider(requireActivity()).get(TrackHealthViewModel::class.java)
+        dbHelper = DataBaseHelper(requireContext())
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         return inflater.inflate(R.layout.fragment_medication, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MedicationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MedicationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dbHelper = DataBaseHelper(requireContext())
+
+        // Set up RecyclerView
+        adapter = MedicationAdapter(dbHelper.getMedicationForPatient(viewModel.patientId))
+        val recyclerView = view.findViewById<RecyclerView>(R.id.medications_recyclerview)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // Set up Add Medication button
+        val addButton = view.findViewById<Button>(R.id.add_medication_button)
+        addButton.setOnClickListener {
+            showAddMedicationDialog()
+        }
+
     }
+
+    private fun showAddMedicationDialog() {
+        val dialog = MedicationDialog(requireContext(), viewModel.patientId, adapter)
+        dialog.show()
+    }
+
 }
+
+
+
---
+++ app/src/main/java/com/example/parkinsonspal/MedicationDialog.kt
@@ -0,0 +1,139 @@
+package com.example.parkinsonspal
+
+import android.app.AlarmManager
+import android.app.Dialog
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Context.ALARM_SERVICE
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
+import android.widget.*
+import com.example.parkinsonspal.Model.DataBaseHelper
+import com.example.parkinsonspal.ReminderBroadcastReceiver
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.ZoneId
+import java.util.*
+
+class MedicationDialog(
+    context: Context,
+    private val patientId: Int,
+    private val medicationAdapter: MedicationAdapter
+) : Dialog(context) {
+
+    private var medicationName: String? = null
+    private var medicationTime: LocalTime? = null
+    private var medicationQuantity: Int? = null
+    private var medicationReminder: String? = null
+    private var medicationTaken: Boolean = false
+
+    private val dbHelper: DataBaseHelper = DataBaseHelper(context)
+    private val ReminderBroadcastReceiver: ReminderBroadcastReceiver = ReminderBroadcastReceiver()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.medication_dialog)
+
+        val window = window
+        window?.setLayout(
+            (context.resources.displayMetrics.widthPixels * 0.9).toInt(),
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+
+        val times = ArrayList<String>()
+        for (hour in 0..23) {
+            for (minute in 0..59 step 15) {
+                val time = String.format("%02d:%02d", hour, minute)
+                times.add(time)
+            }
+        }
+
+        // initialize views
+        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+        val timeSpinner = findViewById<Spinner>(R.id.time_spinner)
+        val quantityEditText = findViewById<EditText>(R.id.quantityEditText)
+
+        val spinnerAdapter = ArrayAdapter<String>(
+            context,
+            android.R.layout.simple_spinner_dropdown_item,
+            times
+        )
+        timeSpinner.adapter = spinnerAdapter
+
+        // set click listener for add button
+        val addButton = findViewById<Button>(R.id.addButton)
+        addButton.setOnClickListener {
+            // save medication data
+            medicationName = nameEditText.text.toString()
+            medicationTime = LocalTime.parse(timeSpinner.selectedItem.toString())
+            medicationQuantity = quantityEditText.text.toString().toIntOrNull()
+
+            // log patientId and medication data
+            Log.d("MedicationDialog", "Patient ID: $patientId")
+            Log.d("MedicationDialog", "Medication Name: $medicationName")
+            Log.d("MedicationDialog", "Medication Time: $medicationTime")
+            Log.d("MedicationDialog", "Medication Quantity: $medicationQuantity")
+
+            // insert medication into database
+            dbHelper.insertMedication(
+                patientId,
+                medicationName!!,
+                medicationTime!!,
+                medicationQuantity!!,
+                medicationTaken
+            )
+
+            // set up a reminder for the medication
+            val reminderTime = LocalTime.parse(medicationTime.toString())
+            val reminderIntent = Intent(context, ReminderBroadcastReceiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(
+                context,
+                0,
+                reminderIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            val alarmManager = context.getSystemService(ALARM_SERVICE) as AlarmManager
+            val now = LocalDateTime.now()
+            val reminderDateTime = LocalDateTime.of(now.toLocalDate(), reminderTime)
+            var reminderMillis = reminderDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
+            if (reminderMillis < System.currentTimeMillis()) {
+                reminderMillis += 86400000 // add 24 hours if reminder time has already passed
+            }
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, reminderMillis, pendingIntent)
+            ReminderBroadcastReceiver.createNotificationChannel(context) // create notification channel
+
+            // notify adapter that data has changed
+            medicationAdapter.notifyDataSetChanged()
+
+            dismiss()
+        }
+
+        val cancelButton = findViewById<Button>(R.id.cancelButton)
+        cancelButton.setOnClickListener {
+
+            dismiss()
+        }
+    }
+
+        fun getMedicationName(): String? {
+        return medicationName
+    }
+
+    fun getMedicationTime(): LocalTime? {
+        return medicationTime
+    }
+
+    fun getMedicationQuantity(): Int? {
+        return medicationQuantity
+    }
+
+    fun getMedicationReminder(): String? {
+        return medicationReminder
+    }
+}
+
+
+
